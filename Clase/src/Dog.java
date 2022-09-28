b5e5121f5c0c9bff6d8602fe18f57f7c60b810d5
Miguel Juan Espinosa <mjuane152@gmail.com>
2022-09-28 12:37:39 +0200

    Dog Final

--- Clase/src/Dog.java
+++ Clase/src/Dog.java
@@ -1,3 +1,16 @@
 public class Dog {
+    //Variables
+    public String name;
+    public int age;
+    public String race;
+    public boolean dangerous;
 
+    //Metodos
+
+    public void displayDog(){
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + age);
+        System.out.println("Raza: " + race);
+        System.out.println("Peligroso: " + dangerous);
+    }
 }
--- Clase/src/Main.java
+++ Clase/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Creacion de objetos
+        Dog dog1 = new Dog();
+
+        //Assignacion de valores
+        dog1.name = "Gilda";
+        dog1.age = 6;
+        dog1.race = "Huscky";
+        dog1.dangerous = false;
+
+        //Llamar metodos
+        dog1.displayDog();
     }
 }
