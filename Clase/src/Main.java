9d663c31be9afad9e341dc64adfcabd1f4a75c9b
mjuan <mjuane152@gmail.com>
2022-09-28 19:21:45 +0200

    Switch

--- Clase/src/Main.java
+++ Clase/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //Tema 2
+        System.out.println("********");
+        System.out.println("TEMA 2");
+        System.out.println("********");
+
         //Creacion de objetos
         Dog dog1 = new Dog();
 
@@ -14,6 +18,10 @@ public class Main {
         dog1.displayDog();
 
         //Tema 7
+        System.out.println("********");
+        System.out.println("TEMA 7");
+        System.out.println("********");
+
         //Asignar valores
         int a = 5;
         int b = 10;
@@ -23,6 +31,8 @@ public class Main {
 
         int i = 2;
         int j = 8;
+
+        int month = 10;
 
         //Metodos
         if (a < b){
@@ -46,6 +56,29 @@ public class Main {
             System.out.println("i es menor a 10 o j es mayor que 9");
         }else {
             System.out.println("Ninguna de las dos variables no cumple el requisito");
+        }
+
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("Hay 31 dias en este mes");
+                break;
+            case 2:
+                System.out.println("Hay 28 dias en este mes");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("Hay 30 dias en este mes");
+                break;
+            default:
+                System.out.println("El mes es invalido");
         }
 
     }
