7748517548262fb3bdcbfcf3064d69f81ee57f03
mjuan <mjuane152@gmail.com>
2022-09-29 07:20:11 +0200

    BasicElements Inicio

---
+++ Clase/src/BasicElements.java
@@ -0,0 +1,190 @@
+public class BasicElements {
+
+    public static void main(String[] args){
+        //Ejercicio 1
+        System.out.println("********");
+        System.out.println("Ejercicio 1");
+        System.out.println("********");
+
+        //Ejercicio 2
+        System.out.println("********");
+        System.out.println("Ejercicio 2");
+        System.out.println("********");
+
+        //Ejercicio 3
+        System.out.println("********");
+        System.out.println("Ejercicio 3");
+        System.out.println("********");
+
+        //Ejercicio 4
+        System.out.println("********");
+        System.out.println("Ejercicio 4");
+        System.out.println("********");
+
+        //Ejercicio 5
+        System.out.println("********");
+        System.out.println("Ejercicio 5");
+        System.out.println("********");
+
+        //Ejercicio 6
+        System.out.println("********");
+        System.out.println("Ejercicio 6");
+        System.out.println("********");
+
+        //Ejercicio 7
+        System.out.println("********");
+        System.out.println("Ejercicio 7");
+        System.out.println("********");
+
+        //Ejercicio 8
+        System.out.println("********");
+        System.out.println("Ejercicio 8");
+        System.out.println("********");
+
+        //Ejercicio 9
+        System.out.println("********");
+        System.out.println("Ejercicio 9");
+        System.out.println("********");
+
+        //Ejercicio 10
+        System.out.println("********");
+        System.out.println("Ejercicio 10");
+        System.out.println("********");
+
+        //Ejercicio 11
+        System.out.println("********");
+        System.out.println("Ejercicio 11");
+        System.out.println("********");
+
+        //Ejercicio 12
+        System.out.println("********");
+        System.out.println("Ejercicio 12");
+        System.out.println("********");
+
+        //Ejercicio 13
+        System.out.println("********");
+        System.out.println("Ejercicio 13");
+        System.out.println("********");
+
+        //Ejercicio 14
+        System.out.println("********");
+        System.out.println("Ejercicio 14");
+        System.out.println("********");
+
+        //Ejercicio 15
+        System.out.println("********");
+        System.out.println("Ejercicio 15");
+        System.out.println("********");
+
+        //Ejercicio 16
+        System.out.println("********");
+        System.out.println("Ejercicio 16");
+        System.out.println("********");
+
+        //Ejercicio 17
+        System.out.println("********");
+        System.out.println("Ejercicio 17");
+        System.out.println("********");
+
+        //Ejercicio 18
+        System.out.println("********");
+        System.out.println("Ejercicio 18");
+        System.out.println("********");
+
+        //Ejercicio 19
+        System.out.println("********");
+        System.out.println("Ejercicio 19");
+        System.out.println("********");
+
+        //Ejercicio 20
+        System.out.println("********");
+        System.out.println("Ejercicio 20");
+        System.out.println("********");
+
+        //Ejercicio 21
+        System.out.println("********");
+        System.out.println("Ejercicio 21");
+        System.out.println("********");
+
+        //Ejercicio 22
+        System.out.println("********");
+        System.out.println("Ejercicio 22");
+        System.out.println("********");
+
+        //Ejercicio 23
+        System.out.println("********");
+        System.out.println("Ejercicio 23");
+        System.out.println("********");
+
+        //Ejercicio 24
+        System.out.println("********");
+        System.out.println("Ejercicio 24");
+        System.out.println("********");
+
+        //Ejercicio 25
+        System.out.println("********");
+        System.out.println("Ejercicio 25");
+        System.out.println("********");
+
+        //Ejercicio 26
+        System.out.println("********");
+        System.out.println("Ejercicio 26");
+        System.out.println("********");
+
+        //Ejercicio 27
+        System.out.println("********");
+        System.out.println("Ejercicio 27");
+        System.out.println("********");
+
+        //Ejercicio 28
+        System.out.println("********");
+        System.out.println("Ejercicio 28");
+        System.out.println("********");
+
+        //Ejercicio 29
+        System.out.println("********");
+        System.out.println("Ejercicio 29");
+        System.out.println("********");
+
+        //Ejercicio 30
+        System.out.println("********");
+        System.out.println("Ejercicio 30");
+        System.out.println("********");
+
+        //Ejercicio 31
+        System.out.println("********");
+        System.out.println("Ejercicio 31");
+        System.out.println("********");
+
+        //Ejercicio 32
+        System.out.println("********");
+        System.out.println("Ejercicio 32");
+        System.out.println("********");
+
+        //Ejercicio 33
+        System.out.println("********");
+        System.out.println("Ejercicio 33");
+        System.out.println("********");
+
+        //Ejercicio 34
+        System.out.println("********");
+        System.out.println("Ejercicio 34");
+        System.out.println("********");
+
+        //Ejercicio 35
+        System.out.println("********");
+        System.out.println("Ejercicio 35");
+        System.out.println("********");
+
+        //Ejercicio 36
+        System.out.println("********");
+        System.out.println("Ejercicio 36");
+        System.out.println("********");
+
+        //Ejercicio 37
+        System.out.println("********");
+        System.out.println("Ejercicio 37");
+        System.out.println("********");
+
+    }
+}
