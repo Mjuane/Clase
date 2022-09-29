76feaea454fa0f6650b8f12d07decf77ada1c42c
mjuan <mjuane152@gmail.com>
2022-09-29 06:28:27 +0200

    BasicOutput 3

--- Clase/src/BasicOutput.java
+++ Clase/src/BasicOutput.java
@@ -5,75 +5,97 @@ public class BasicOutput {
         System.out.println("********");
         System.out.println("Ejercicio 1");
         System.out.println("********");
+        //Solución
+        System.out.println("Hello!");
 
         //Ejercicio 2
         System.out.println("********");
         System.out.println("Ejercicio 2");
         System.out.println("********");
+        //Solución
+        String var01 = "Hello!";
+        System.out.printf("%s", var01);;
+        System.out.println();
 
         //Ejercicio 3
         System.out.println("********");
         System.out.println("Ejercicio 3");
         System.out.println("********");
+        //Solución
+        String var02 = "How are you?";
+        System.out.printf("My friend! %s %s", var01, var02);
+        System.out.println();
 
         //Ejercicio 4
         System.out.println("********");
         System.out.println("Ejercicio 4");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 5
         System.out.println("********");
         System.out.println("Ejercicio 5");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 6
         System.out.println("********");
         System.out.println("Ejercicio 6");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 7
         System.out.println("********");
         System.out.println("Ejercicio 7");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 8
         System.out.println("********");
         System.out.println("Ejercicio 8");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 9
         System.out.println("********");
         System.out.println("Ejercicio 9");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 10
         System.out.println("********");
         System.out.println("Ejercicio 10");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 11
         System.out.println("********");
         System.out.println("Ejercicio 11");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 12
         System.out.println("********");
         System.out.println("Ejercicio 12");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 13
         System.out.println("********");
         System.out.println("Ejercicio 13");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 14
         System.out.println("********");
         System.out.println("Ejercicio 14");
         System.out.println("********");
+        //Solución
 
         //Ejercicio 15
         System.out.println("********");
         System.out.println("Ejercicio 15");
         System.out.println("********");
+        //Solución
     }
 }
