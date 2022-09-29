c3d0b9d3a6f743510cb82891473545c02b8da6f9
mjuan <mjuane152@gmail.com>
2022-09-29 06:16:13 +0200

    BasicOutput Inicio

---
+++ Clase/src/BasicOutput.java
@@ -0,0 +1,79 @@
+public class BasicOutput {
+
+    public static void main(String[] args) {
+        //Ejercicio 1
+        System.out.println("********");
+        System.out.println("Ejercicio 1");
+        System.out.println("********");
+
+        //Ejercicio 2
+        System.out.println("********");
+        System.out.println("Ejercicio 2");
+        System.out.println("********");
+
+        //Ejercicio 3
+        System.out.println("********");
+        System.out.println("Ejercicio 3");
+        System.out.println("********");
+
+        //Ejercicio 4
+        System.out.println("********");
+        System.out.println("Ejercicio 4");
+        System.out.println("********");
+
+        //Ejercicio 5
+        System.out.println("********");
+        System.out.println("Ejercicio 5");
+        System.out.println("********");
+
+        //Ejercicio 6
+        System.out.println("********");
+        System.out.println("Ejercicio 6");
+        System.out.println("********");
+
+        //Ejercicio 7
+        System.out.println("********");
+        System.out.println("Ejercicio 7");
+        System.out.println("********");
+
+        //Ejercicio 8
+        System.out.println("********");
+        System.out.println("Ejercicio 8");
+        System.out.println("********");
+
+        //Ejercicio 9
+        System.out.println("********");
+        System.out.println("Ejercicio 9");
+        System.out.println("********");
+
+        //Ejercicio 10
+        System.out.println("********");
+        System.out.println("Ejercicio 10");
+        System.out.println("********");
+
+        //Ejercicio 11
+        System.out.println("********");
+        System.out.println("Ejercicio 11");
+        System.out.println("********");
+
+        //Ejercicio 12
+        System.out.println("********");
+        System.out.println("Ejercicio 12");
+        System.out.println("********");
+
+        //Ejercicio 13
+        System.out.println("********");
+        System.out.println("Ejercicio 13");
+        System.out.println("********");
+
+        //Ejercicio 14
+        System.out.println("********");
+        System.out.println("Ejercicio 14");
+        System.out.println("********");
+
+        //Ejercicio 15
+        System.out.println("********");
+        System.out.println("Ejercicio 15");
+        System.out.println("********");
+    }
+}
