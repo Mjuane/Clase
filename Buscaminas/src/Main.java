4bfeca15500a87708cb361714cfbd02d8b47a114
Miguel Juan Espinosa <mjuane152@gmail.com>
2022-10-04 08:33:35 +0200

    Creacion de tablero

--- Buscaminas/src/Main.java
+++ Buscaminas/src/Main.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
 
+public class Main {
+
+
+    public static void main(String[] args) {
+        tablero();
+    }
+
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+    public static void tablero(){
+        int filas = 14;
+        int columnas = 18;
+
+        for (int i = 0; i < filas; i++){
+            for (int k = 0; k < columnas;k++){
+                System.out.print(ANSI_CYAN + "0 " + ANSI_CYAN);
+            }
+            System.out.println(" ");
+        }
     }
 }
