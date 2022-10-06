c14446cc83c57e3048b938178a6177841fa06e1b
Miguel Juan Espinosa <mjuane152@gmail.com>
2022-10-06 11:44:16 +0200

    Mitja Aritmetica acabada

--- ExamenPrueba/src/Main.java
+++ ExamenPrueba/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] m = {5, 6, 3, 5, 3, 8, 1, 5};
+
+        System.out.println(mitjaAritmetica(m));
+    }
+
+    public static double mitjaAritmetica(int[] m){
+        double mitja = 0;
+        int temp = 0;
+
+        for (int i = 0; i < m.length;i++){
+            temp += m[i];
+        }
+
+        mitja = (double) temp/m.length;
+
+        return mitja;
     }
 }
