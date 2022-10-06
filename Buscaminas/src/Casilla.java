3050f11315c7909e80c6335fdd1e202761acc0bd
Miguel Juan Espinosa <mjuane152@gmail.com>
2022-10-06 09:51:30 +0200

    Pasar a objetos a medias

---
+++ Buscaminas/src/Casilla.java
@@ -0,0 +1,13 @@
+public class Casilla {
+    //Variables
+    private int fila;
+    private int columna;
+    private boolean tapado;
+    private final char porDefecto = '-';
+
+    //Metodos
+
+    public void setFila(int x){
+        this.fila = x;
+    }
+}
