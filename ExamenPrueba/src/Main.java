d498dcc7f05d280c39acecf168fad6d3878d3de0
Miguel Juan Espinosa <mjuane152@gmail.com>
2022-10-06 12:03:00 +0200

    Valor Minimo acabado

--- ExamenPrueba/src/Main.java
+++ ExamenPrueba/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         int[] m = {5, 6, 3, 5, 3, 8, 1, 5};
 
         System.out.println(mitjaAritmetica(m));
+        System.out.println(valorMinimo(m));
     }
 
     public static double mitjaAritmetica(int[] m){
@@ -16,5 +17,16 @@ public class Main {
         mitja = (double) temp/m.length;
 
         return mitja;
+    }
+
+    public static int valorMinimo(int[] m){
+        int minimo = Integer.MAX_VALUE;
+
+        for (int i = 0; i < m.length; i++) {
+            if (minimo > m[i]){
+                minimo = m[i];
+            }
+        }
+        return minimo;
     }
 }
