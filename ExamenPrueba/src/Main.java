4164297cfa469f8201e1ae918bc351f80d278788
Miguel Juan Espinosa <mjuane152@gmail.com>
2022-10-06 12:12:07 +0200

    Valor e index maximos acabados

--- ExamenPrueba/src/Main.java
+++ ExamenPrueba/src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
         int[] m = {5, 6, 3, 5, 3, 8, 1, 5};
 
-        System.out.println(mitjaAritmetica(m));
-        System.out.println(valorMinimo(m));
-        System.out.println(indexMinimo(m));
+        System.out.println("Media Artimetica: " + mitjaAritmetica(m));
+        System.out.println("Valor minimo: " + valorMinimo(m));
+        System.out.println("Index minimo: " + indexMinimo(m));
+        System.out.println("Valor maximo: " + valorMaximo(m));
+        System.out.println("Index maximo: " + indexMaximo(m));
     }
 
     public static double mitjaAritmetica(int[] m){
@@ -36,6 +38,28 @@ public class Main {
 
         for (int i = 0; i < m.length; i++) {
             if (minimo > m[i]){
+                minimo = m[i];
+                indexMin = i;
+            }
+        }
+        return indexMin;
+    }
+    public static int valorMaximo(int[] m){
+        int minimo = Integer.MIN_VALUE;
+
+        for (int i = 0; i < m.length; i++) {
+            if (minimo < m[i]){
+                minimo = m[i];
+            }
+        }
+        return minimo;
+    }
+    public static int indexMaximo(int[] m){
+        int minimo = Integer.MIN_VALUE;
+        int indexMin = 0;
+
+        for (int i = 0; i < m.length; i++) {
+            if (minimo < m[i]){
                 minimo = m[i];
                 indexMin = i;
             }
